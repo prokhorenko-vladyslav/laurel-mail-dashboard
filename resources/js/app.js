700dab985d123cf6589ccf781ee49793de9936d8
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2021-02-13 00:33:58 +0200

    Refactored project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-require('./bootstrap');
+import Vue from 'vue';
+import router from "./plugins/router";
+import store from "./plugins/store";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+
+Vue.prototype.axios = axios.create({
+    baseURL: process.env.API_URL,
+    timeout: 1000,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+});
+
+const app = new Vue({
+    router,
+    store,
+    vuetify
+}).$mount('#app');
---
+++ resources/views/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mail dashboard</title>
+</head>
+<body>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+    <script src="{{ asset('css/app.css') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
